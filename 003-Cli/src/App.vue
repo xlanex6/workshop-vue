74e9e0aa2269e0c475ad7e980744525c484c4af3
Alex DUVAL <alexduval71@gmail.com>
2020-07-03 15:19:13 +0200

    Vue cli start

---
+++ 003-Cli/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Annuaire Utilisateurs</h1>
+    <p>Mon favori est : {{favorite}}</p>
+    <button> Recuperer les utilisateurs</button>
+
+    <div id="users-list">
+      USERS ....
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  max-width: 978px;
+  margin: auto;
+}
+#users-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
